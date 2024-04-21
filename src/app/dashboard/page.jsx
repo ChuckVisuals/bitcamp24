f5a29518459078612879ac0c5ce92f664672aba8
Chuck <firstinmath2@gmail.com>
2024-04-21 05:06:28 -0400

    deplot fix

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -19,9 +19,10 @@ export default function Dashboard() {
     const [data, setData] = useState([]);
     const [input, setInput] = useState('');
     const [showForm, setShowForm] = useState(false);
+    const [ShowDeleteForm, setShowDeleteForm] = useState(false);
     const [formdata, setFormData] = useState({});
 
-    //form data states 
+    //form data states for adding/deleting chore
     const [groupName, setGroupName] = useState('');
     const [dueDate, setDueDate] = useState('');
     const [name, setName] = useState('');
@@ -96,6 +97,11 @@ export default function Dashboard() {
         setShowForm(true);
     };
 
+    const DeleteChore = () => {
+        setShowDeleteForm(true);
+
+    };
+
     const handleSubmitForm = (event) => {
         event.preventDefault();
         // Handle form submission
@@ -113,24 +119,44 @@ export default function Dashboard() {
         setShowForm(false);
         setFormData(chore);
         setInput(input) //used to refresh the page
+    };
+
+    const handleDeleteSubmitForm = (event) => {
+        event.preventDefault();
+
+        //remove from data when it finds the same name and group_name
+        console.log('data', data);
+        setData(prevData => prevData.filter(chore => chore.name != name));
+        console.log('data', data);
+
+        //remove from database
+        fetch(`https://bitcamp-backend.vercel.app/deletechore?group_name=${groupName}&name=${name}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log('Deleted:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
 
 
+        setShowDeleteForm(false);
     };
 
     return (
         <div className="bg-stone-900 w-screen min-h-screen pt-32 px-16">
 
             {/* Buttons for dashboard */}
-            <div className="flex justify-between">
-                <form onSubmit={handleSubmit} className="text-white mb-16">
-                    <input name="input" type="text" placeholder='Enter group name' className="bg-gray-800 p-2 rounded" />
+            <div className="flex justify-between items-center bg-stone-950 border border-gray-800 rounded-3xl p-4 mb-6">
+                <form onSubmit={handleSubmit} className="text-white ">
+                    <input name="input" type="text" placeholder="Enter group name" className="bg-gray-800 p-2 rounded" />
                     <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
                 </form>
                 <div>
-                    <h1 className="text-4xl text-white mb-8 justify-center items-center flex">DashBoard</h1>
+                    <h1 className="text-4xl text-white justify-center items-center flex">DashBoard</h1>
                 </div>
-                <div className="mb-4 space-x-5">
-                    <button onClick={handleTodayChores} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2">
+                <div className=" gap-2 grid grid-cols-2 justify-center items-center">
+                    <button onClick={handleTodayChores} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
                         Today's Chores
                     </button>
                     <button onClick={handleTomorrowChores} className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">
@@ -138,6 +164,9 @@ export default function Dashboard() {
                     </button>
                     <button onClick={AddChore} className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
                         Add Chore
+                    </button>
+                    <button onClick={DeleteChore} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                        Delete Chore
                     </button>
                 </div>
             </div>
@@ -193,6 +222,33 @@ export default function Dashboard() {
                             </button>
                             <button
                                 onClick={() => setShowForm(false)}
+                                className=" bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
+                                Cancel
+                            </button>
+                        </div>
+
+                    </form>
+                </div>
+            )}
+
+            {ShowDeleteForm && (
+                <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-md">
+                    <form onSubmit={handleDeleteSubmitForm} className="bg-gray-900 p-4 rounded mt-20">
+
+                        <div className="flex flex-col gap-10">
+                            <label className='flex items-center'>
+                                Group Name:
+                                <input name="group_name" type="text" onChange={(e) => setGroupName(e.target.value)} className="bg-gray-800 p-2 rounded" />
+                            </label>
+                            <label className='flex items-center'>
+                                Name:
+                                <input name="name" type="text" onChange={(e) => setName(e.target.value)} className="bg-gray-800 p-2 rounded" />
+                            </label>
+                            <button type="submit" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                                Submit
+                            </button>
+                            <button
+                                onClick={() => setShowDeleteForm(false)}
                                 className=" bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
                                 Cancel
                             </button>
